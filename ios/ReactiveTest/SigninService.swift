4c1bf0cb830bde29226a71c7a1150b3d4ccff9de
Giuseppe Basile <giuseppe@novoda.com>
2019-09-30 11:06:15 +0200

    authentication call

--- ios/ReactiveTest/SigninService.swift
+++ ios/ReactiveTest/SigninService.swift
@@ -21,15 +21,35 @@ enum SigninServiceError: Error {
 
 class SigninService {
     fileprivate static let decoder = JSONDecoder()
+    fileprivate static let encoder = JSONEncoder()
     
     func authenticate(username: String, password: String) -> Single<AuthResponse> {
-        guard let url = URL(string: "localhost:8081") else {
+        guard let url = URL(string: "http://localhost:8081/register") else {
             return Single.error(SigninServiceError.invalidRequest)
         }
         
-        let request = URLRequest(url: url)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = [
+            "username": username,
+            "password": password
+            ].toJSONData(encoder: SigninService.encoder)
         return URLSession.shared.rx.data(request: request)
             .asSingle()
             .map { try SigninService.decoder.decode(AuthResponse.self, from: $0) }
     }
+}
+
+protocol JSONEncodable: Encodable {
+    func toJSONData(encoder: JSONEncoder) -> Data?
+}
+
+extension JSONEncodable {
+    func toJSONData(encoder: JSONEncoder) -> Data? {
+        return try? encoder.encode(self)
+    }
+}
+
+extension Dictionary: JSONEncodable where Key: Encodable, Value: Encodable {
+    
 }
