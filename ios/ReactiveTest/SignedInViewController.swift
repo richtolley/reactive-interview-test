78add902a1abe645ef92c9bf351c9ae3b77c8994
Richard Tolley <richard@novoda.com>
2019-09-30 10:07:49 +0200

    setup test backend

---
+++ ios/ReactiveTest/SignedInViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignedInViewController.swift
+//  ReactiveTest
+//
+//  Created by Richard Tolley on 30/09/2019.
+//  Copyright © 2019 Richard Tolley. All rights reserved.
+//
+
+import Foundation
--- node/server.js
+++ node/server.js
@@ -1,17 +1,64 @@
 const express = require('express');
+const crypto = require('crypto-js');
+const fs = require('fs')
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('{"value": "wrong"}');
-   let responseObject = {};
-   responseObject.value = "wrong";
-   res.send(JSON.stringify(responseObject))
+   res.send('Reactive Test Backend');
 })
 
-app.get('/thing', (req, res) => {
-	let responseObject = {};
-	responseObject.secret = "nope";
-	res.send(JSON.stringify(responseObject))
+let usernameValid = (name) => {
+	return name.length > 2 && name.length <= 20
+}
+
+let passwordValid = (password) => {
+	let hasNum = /\d/
+	return password.length > 4 && password.length <= 20 && hasNum.test(password)
+}
+
+app.post('/register', (req, res) => {
+
+	console.log(req);
+	let username = req.headers.username
+	let password = req.headers.password
+
+	if(usernameValid(username) && passwordValid(password)) {
+		let responseObject = {}
+		responseObject.authorization = crypto.SHA256(username + password).toString()
+
+		let db = {}
+		db[responseObject.authorization] = true
+
+		fs.writeFile("db.json", JSON.stringify(db), (error) => {
+			if(error) {
+				res.status(500).send("Internal Server Error" + error);
+			} else {
+				res.send(responseObject);
+			}
+		})
+
+	} else {
+		res.status(400).send("Invalid name or password");
+	}
+})
+
+app.post('/newsletter', (req, res) => {
+	let token = req.headers.authorization
+
+	fs.readFile("db.json", (error, file) => {
+		if(error) {
+			res.status(500).send("Invalid token db")
+		} else {
+			let db = JSON.parse(file)
+			if(db[token] === true) {
+				let responseObject = {}
+				responseObject.message = "Happy days, you are subscribed";
+				res.send(responseObject)
+			} else {
+				res.status(400).send("Invalid token");
+			}
+		}
+	})
 })
 
 const server = app.listen(8081, () => {
