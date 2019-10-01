5aceb64750723ee01dc6584eef8cb813e7e45d1d
Richard Tolley <richard@novoda.com>
2019-10-01 16:37:51 +0200

    simplify signin service

--- ios/ReactiveTest/SigninService.swift
+++ ios/ReactiveTest/SigninService.swift
@@ -50,31 +50,19 @@ class SigninService {
             .asSingle()
             .map { try SigninService.decoder.decode(AuthResponse.self, from: $0) }
     }
-}
 
-protocol JSONEncodable: Encodable {
-    func toJSONData(encoder: JSONEncoder) -> Data?
-}
-
-extension JSONEncodable {
-    func toJSONData(encoder: JSONEncoder) -> Data? {
-        return try? encoder.encode(self)
-    }
-}
-
-extension Dictionary: JSONEncodable where Key: Encodable, Value: Encodable {
-    func subscribeToTheNewsletter(token: String) -> Completable {
+    func newsletter(authorization: String) -> Single<MessageResponse> {
         guard let url = URL(string: urlString + "newsletter") else {
-            return Completable.error(SigninServiceError.invalidRequest)
+            return Single.error(SigninServiceError.invalidRequest)
         }
-        
+
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.allHTTPHeaderFields = [
-            "authorization": token
+          "Authorization": authorization,
         ]
-        
         return URLSession.shared.rx.data(request: request)
-            .ignoreElements()
+            .asSingle()
+            .map { try SigninService.decoder.decode(MessageResponse.self, from: $0) }
     }
 }
